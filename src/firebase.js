398611297361e57498968f1beb1831b8ac427d25
Suleiman <mdsuleiman.edu@gmail.com>
2026-05-09 17:48:36 +0530

    updated feature

--- src/firebase.js
+++ src/firebase.js
@@ -4,26 +4,46 @@ import {
   getFirestore,
   collection,
   addDoc,
+  getDocs,
+  updateDoc,
+  doc,
   onSnapshot,
 } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAO9_MvfIgJlho_4uZmYAR0Bl82yLVmB1M",
-  authDomain: "roadscan-project.firebaseapp.com",
-  projectId: "roadscan-project",
-  storageBucket: "roadscan-project.firebasestorage.app",
-  messagingSenderId: "54499287539",
-  appId: "1:54499287539:web:ba26ea9838b1de50337286"
+  apiKey:
+    'AIzaSyAO9_MvfIgJlho_4uZmYAR0Bl82yLVmB1M',
+
+  authDomain:
+    'roadscan-project.firebaseapp.com',
+
+  projectId:
+    'roadscan-project',
+
+  storageBucket:
+    'roadscan-project.firebasestorage.app',
+
+  messagingSenderId:
+    '54499287539',
+
+  appId:
+    '1:54499287539:web:ba26ea9838b1de50337286',
 }
 
 const app =
-  initializeApp(firebaseConfig)
+  initializeApp(
+    firebaseConfig
+  )
 
-const db = getFirestore(app)
+const db =
+  getFirestore(app)
 
 export {
   db,
   collection,
   addDoc,
+  getDocs,
+  updateDoc,
+  doc,
   onSnapshot,
 }
